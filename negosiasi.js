ad41a741c649ad21c56bb6b467ba41a641540a5d
andys171717-crypto <andys171717@gmail.com>
2026-06-20 20:48:03 +0700

    Integrate Firebase and load request data

--- negosiasi.js
+++ negosiasi.js
@@ -1,3 +1,56 @@
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+
+import {
+getFirestore,
+doc,
+getDoc
+}
+from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyCnk56ZY63q2h1ewEdiivzB0rrSfJOJtYo",
+authDomain: "jasaku-92b55.firebaseapp.com",
+projectId: "jasaku-92b55",
+storageBucket: "jasaku-92b55.firebasestorage.app",
+messagingSenderId: "217601622524",
+appId: "1:217601622524:web:e3bc48dbdc50d7cb10b279"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const params =
+new URLSearchParams(
+window.location.search
+);
+
+const requestId =
+params.get("id");
+
+async function loadRequest(){
+
+if(!requestId){
+return;
+}
+
+const requestRef =
+doc(
+db,
+"requests",
+requestId
+);
+
+const requestSnap =
+await getDoc(requestRef);
+
+if(!requestSnap.exists()){
+return;
+}
+
+const data =
+requestSnap.data();
+
 document.getElementById(
 "requestInfo"
 ).innerHTML = `
@@ -8,38 +61,44 @@ Kode Permintaan
 
 <br>
 
-JKR2606200001
+${data.requestCode || "-"}
 
 </div>
 
 <h2>
-Bengkel Bubut
+${data.namaJasa || "-"}
 </h2>
 
 <p>
-Customer: Tere
+Customer:
+${data.nama || "-"}
+</p>
+
+<p>
+Alamat:
+${data.alamat || "-"}
+</p>
+
+<p>
+Keluhan:
+${data.keluhan || "-"}
+</p>
+
+<p>
+Status:
+${data.status || "-"}
 </p>
 
 `;
 
+}
+
+loadRequest();
+
 document.getElementById(
 "chatMessages"
 ).innerHTML = `
-
 <div class="message customer">
-Bang bisa malam ini?
+Ruang negosiasi sedang disiapkan...
 </div>
-
-<div class="message provider">
-Bisa.
-</div>
-
-<div class="message customer">
-Estimasi berapa?
-</div>
-
-<div class="message provider">
-Sekitar 250 ribu.
-</div>
-
 `;
