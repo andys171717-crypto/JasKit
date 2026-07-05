3e2ca52380ee5ca717d16784e5e4f84c8e8dd42d
Andy <andys171717@gmail.com>
2026-07-05 15:47:05 +0700

    Rename pilih-lokasi js.txt to pilih-lokasi.js

---
+++ pilih-lokasi.js
@@ -0,0 +1,157 @@
+window.map = L.map("map").setView(
+[-6.2088,106.8456],
+13
+);
+
+L.tileLayer(
+"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+{
+
+maxZoom:19,
+
+attribution:"© OpenStreetMap"
+
+}
+
+).addTo(window.map);
+
+const marker =
+L.marker(
+[-6.2088,106.8456],
+{
+
+draggable:true
+
+}
+
+).addTo(window.map);
+
+let selectedLatitude =
+-6.2088;
+
+let selectedLongitude =
+106.8456;
+
+let selectedAddress =
+"Belum memilih lokasi.";
+
+const addressBox =
+document.getElementById(
+"selectedAddress"
+);
+
+if(navigator.geolocation){
+
+navigator.geolocation.getCurrentPosition(
+
+(position)=>{
+
+const lat =
+position.coords.latitude;
+
+const lng =
+position.coords.longitude;
+
+selectedLatitude =
+lat;
+
+selectedLongitude =
+lng;
+
+window.map.setView(
+[lat,lng],
+16
+);
+
+marker.setLatLng(
+[lat,lng]
+);
+
+updateAddress(
+lat,
+lng
+);
+
+},
+
+()=>{
+
+console.log(
+"GPS ditolak user"
+);
+
+}
+
+);
+
+}
+
+async function updateAddress(
+lat,
+lng
+){
+
+try{
+
+const response =
+await fetch(
+
+`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`
+
+);
+
+const data =
+await response.json();
+
+selectedAddress =
+data.display_name ||
+"Alamat tidak ditemukan";
+
+addressBox.textContent =
+selectedAddress;
+
+}catch(error){
+
+addressBox.textContent =
+"Gagal mengambil alamat.";
+
+}
+
+}
+
+marker.on(
+
+"dragend",
+
+()=>{
+
+const position =
+marker.getLatLng();
+
+selectedLatitude =
+position.lat;
+
+selectedLongitude =
+position.lng;
+
+updateAddress(
+
+selectedLatitude,
+
+selectedLongitude
+
+);
+
+console.log(
+
+"Marker dipindahkan:",
+
+selectedLatitude,
+
+selectedLongitude
+
+);
+
+}
+
+);
